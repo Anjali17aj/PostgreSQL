9db7854807dea4f2d3e55e878ad378c26ae89b8c
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-23 20:20:07 +0530

    SQL Commands

---
+++ 4. SQL Commands/SQL Where clause.sql
@@ -0,0 +1,4 @@
+select first_name from customer_table where age=46;
+select * from customer_table where first_name='Anjali';
+Select distinct first_name from customer_table where first_name='Anjali';
+select first_name from customer_table where age>15;
---
+++ 4. SQL Commands/select distinct.sql
@@ -0,0 +1,3 @@
+
+select distinct first_name, age from customer_table;
+select distinct * from customer_table;
