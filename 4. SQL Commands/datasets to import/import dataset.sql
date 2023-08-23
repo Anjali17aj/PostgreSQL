80001f00cb631ead5508efe64cc536580f15182c
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-23 19:11:47 +0530

    SQL Commands

---
+++ 4. SQL Commands/datasets to import/import dataset.sql
@@ -0,0 +1,12 @@
+insert into customer_table values
+(1, 'Anjali', 'Kumari',23,'sayhellotoanjali@gmail.com');
+
+insert into customer_table (cust_id, first_name,age,email_id) values(2,'Chiky',20,'Ashwinichiky18@gmail.com');
+
+insert into customer_table values(3,'Ajay','Ram',46,'ram.ajay175@gmail.com'),
+(4,'Anita','Ram',36,'anita.rm305@gmail.com'),
+(5,'Ashwini','Kumar',18,'ash18@gmail.com'),
+(6,'Prachi','Kumari',22,'prachi.aj1707@gmail.com');
+
+copy customer_table from 'C:\Users\Anjali\Desktop\SQL\4. SQL Commands\datasets to import\copy.csv' delimiter ',' csv header;
+copy customer_table from 'C:\Users\Anjali\Desktop\SQL\4.SQL Commands\datasets to import\copytext.txt' delimiter ',';
