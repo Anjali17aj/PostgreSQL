e34f59b0e3b192451974d7b47f1eec0e93d710e2
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-23 20:34:05 +0530

    SQL Commands

---
+++ 4. SQL Commands/Update.sql
@@ -0,0 +1,4 @@
+select * from customer_table;
+update customer_table set email_id = 'Zoya@gmail.com' where first_name='Prachi';
+update customer_table set first_name = 'Siya', last_name='Ram' where first_name='Ash';
+select * from customer_table;
