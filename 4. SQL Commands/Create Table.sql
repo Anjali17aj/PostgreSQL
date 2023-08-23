94456d95e87e8c9a248d76e205bf855121ab8197
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-23 14:28:34 +0530

    SQL Commands

---
+++ 4. SQL Commands/Create Table.sql
@@ -0,0 +1,6 @@
+create table Customer_table (
+	Cust_id int,
+	First_name varchar,
+	Last_name varchar,
+	age int,
+	email_id varchar);
---
+++ 4. SQL Commands/Insert Into Table.sql
@@ -0,0 +1,9 @@
+insert into customer_table values
+(1, 'Anjali', 'Kumari',23,'sayhellotoanjali@gmail.com');
+
+insert into customer_table (cust_id, first_name,age,email_id) values(2,'Chiky',20,'Ashwinichiky18@gmail.com');
+
+insert into customer_table values(3,'Ajay','Ram',46,'ram.ajay175@gmail.com'),
+(4,'Anita','Ram',36,'anita.rm305@gmail.com'),
+(5,'Ashwini','Kumar',18,'ash18@gmail.com'),
+(6,'Prachi','Kumari',22,'prachi.aj1707@gmail.com');
