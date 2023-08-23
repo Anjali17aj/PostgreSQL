ebca34d7ffef05dc18fa6ec2adc92c4d12a4f890
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-23 20:19:15 +0530

    SQL Commands

---
+++ 4. SQL Commands/logical conjunctive operators.sql
@@ -0,0 +1,3 @@
+select first_name, last_name, age from customer_table where age>20 and age<30;
+select first_name, last_name, age from customer_table where age>20 or age<30;
+select first_name, last_name, age from customer_table where not age=25;
